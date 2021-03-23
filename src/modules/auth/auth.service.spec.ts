375de23a62004a53ccf03812882626c120dea5eb
Wilk Franca <wilk.caetano@gmail.com>
2021-03-22 23:36:14 -0300

    fix: corrections

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from 'src/app.module';
 import { AuthModule } from './auth.module';
 import { AuthService } from './auth.service';
 
@@ -8,7 +7,7 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule, AppModule],
+      imports: [AuthModule],
       providers: [AuthService],
     }).compile();
 
