b41d9c5ab2c33af9c1d531bb66255166822ad61d
Wilk Franca <wilk.caetano@gmail.com>
2021-03-30 01:09:33 -0300

    fix: news package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AvatarUserController } from './modules/avatar-user/avatar-user.controller';
-import { MailModule } from './modules/mail/mail.module';
 import { UsersModule } from './modules/users/users.module';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
-    MailModule,
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
